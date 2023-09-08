4cbba4dd409f2f420e886f32b6b4606396c41770
switt_tv <4galarzajuan2004@gmail.com>
2023-09-07 22:04:46 -0600

    borrando todo

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true})
-</script>
-
-<style>
-    /* your styles go here */
-</style>
-
-<!-- markup (zero or more items) goes here -->
